c486b1ac4c0429c5dc59efe442de81d13c639ce0
Matheus Isquierdo <matheus.isquierdo@fourd.com.br>
2024-08-17 23:10:51 -0300

    Add log body

--- app.js
+++ app.js
@@ -13,32 +13,32 @@ app.get('/webhook/', function (req, res) {
 })
 
 app.post("/webhook", (req, res) => {
-  console.log(req.body);
+  console.log(JSON.stringify(req.body, null, 2));
   // Parse the request body from the POST
   let body = req.body;
 
-  // info on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
-  if (req.body.object) {
-    if (
-      req.body.entry &&
-      req.body.entry[0].changes &&
-      req.body.entry[0].changes[0] &&
-      req.body.entry[0].changes[0].value.messages &&
-      req.body.entry[0].changes[0].value.messages[0]
-    ) {
+  // // info on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
+  // if (req.body.object) {
+  //   if (
+  //     req.body.entry &&
+  //     req.body.entry[0].changes &&
+  //     req.body.entry[0].changes[0] &&
+  //     req.body.entry[0].changes[0].value.messages &&
+  //     req.body.entry[0].changes[0].value.messages[0]
+  //   ) {
 
-      // do your stuff here.....
+  //     // do your stuff here.....
 
-      let phone_number_id =
-        req.body.entry[0].changes[0].value.metadata.phone_number_id;
-      let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
-      let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
-    }
+  //     let phone_number_id =
+  //       req.body.entry[0].changes[0].value.metadata.phone_number_id;
+  //     let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
+  //     let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
+  //   }
     res.sendStatus(200);
-  } else {
-    // Return a '404 Not Found' if event is not from a WhatsApp API
-    res.sendStatus(404);
-  }
+  // } else {
+  //   // Return a '404 Not Found' if event is not from a WhatsApp API
+  //   res.sendStatus(404);
+  // }
 });
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
